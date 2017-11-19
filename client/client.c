8669886e47c37215042dd9dac8a8d23df8bcf555
Brian Byrne <bbyrne5@nd.edu>
2017-11-19 14:39:21 -0500

    client

--- client/client.c
+++ client/client.c
@@ -6,12 +6,54 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 void private_message();
 void broadcast();
 void *handle_messages(void *);
 
 int main(int argc, char *argv[]) { 
+  int s; 
+  struct hostent *hp;
+  struct sockaddr_in sin;
+
+  if (argc != 4){
+    fprintf(stderr, "usage: ./client server port username");
+    exit(1);
+  }
+
+  char* host = argv[1];
+  int port = atoi(argv[2]);
+  char* username = argv[3];
+
+  // translate host name into IP address
+  hp = gethostbyname(host);
+  if(!hp) {
+    fprintf(stderr, "client: unknown host: %s\n", host);
+    exit(1);
+  }
+
+  // build address
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+  sin.sin_port = htons(port);
+
+  // active open
+  if ((s=socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("client: socket error");
+    exit(1);
+  }
+
+  if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
+    perror("client: connect failed");
+    close(s);
+    exit(1);
+  } 
+
   while (!EXIT) {
     // Log in // 
     //login(username)
