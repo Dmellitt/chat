b0da376c6fc443297ee9265ad427b9d12705124e
David Ryan Mellitt <dmellitt@student00.cse.nd.edu>
2017-11-19 23:29:12 -0500

    server

--- server/server.c
+++ server/server.c
@@ -40,10 +40,10 @@ void del(struct node * current)
 }
 
 struct node root;
-struct node * newest = &root;
 
 int main(int argc , char *argv[])
 {
+  root.socket = -1;
   int socket_desc , client_sock , c, port;
   struct sockaddr_in server , client;
 
@@ -60,7 +60,6 @@ int main(int argc , char *argv[])
   {
     printf("Could not create socket");
   }
-  puts("Socket created");
 
   //Prepare the sockaddr_in structure
   server.sin_family = AF_INET;
@@ -74,29 +73,28 @@ int main(int argc , char *argv[])
     perror("bind failed. Error");
     return 1;
   }
-  puts("bind done");
 
   //Listen
   listen(socket_desc , 3);
 
   //Accept and incoming connection
-  puts("Waiting for incoming connections...");
   c = sizeof(struct sockaddr_in);
   pthread_t thread_id;
 
   while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
   {
-    newest->socket = client_sock;
-    newest->next = malloc(sizeof(struct node));
-    if(newest->next == NULL){
+    struct node * current = &root;
+    while(current->next)
+      current = current->next;
+
+    current->next = malloc(sizeof(struct node));
+    if(current->next == NULL){
       printf("Error allocating user node\n");
       return 1;
     }
-    newest = newest->next;
-    newest->socket = -1;
-    newest->next = NULL;
-
-    puts("Connection accepted");
+    current = current->next;
+    current->socket = client_sock;
+    current->next = NULL;
 
     if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
     {
@@ -104,9 +102,6 @@ int main(int argc , char *argv[])
       return 1;
     }
 
-    //Now join the thread , so that we dont terminate before the thread
-    //pthread_join( thread_id , NULL);
-    puts("Handler assigned");
   }
 
   if (client_sock < 0)
@@ -127,39 +122,183 @@ void *connection_handler(void *socket_desc)
   //Get the socket descriptor
   int sock = *(int*)socket_desc;
   int read_size;
-  char * username, buf[BUFSIZE];
+  char * username, buf[10];
 
   if(login(sock) < 0)
     return 0;
 
   while(1) {
-    if(recv(sock , buf, 1 , 0) < 0)
+    if(recv(sock , buf, 10 , 0) < 0)
     {
       printf("Error reading\n");
       return 0;
     }
 
-    if(buf[0] == 'B')
-      broadcast(sock);
-    else if(buf[0] == 'P')
-      private(sock);
-    else if (buf[0] == 'E')
+    if(buf[0] == 'B') {
+      if(broadcast(sock) < 0)
+        return 0;
+    }
+    else if(buf[0] == 'P') {
+      if(private(sock) < 0)
+        return 0;
+    }
+    else if (buf[0] == 'E') {
       end(sock);
+      return 0;
+    }
   }
 }
 
 int broadcast(int sock)
 {
+  int read_size;
+  char buf[BUFSIZE];
+  char * message = "0";
+
+  if(send(sock , message , strlen(message)+1, 0) < 0) {
+    printf("Error writing\n");
+    return -1;
+  }
+  if(send(sock , message , strlen(message)+1, 0) < 0) {
+    printf("Error writing\n");
+    return -1;
+  }
+
+  buf[0] = 'D';
+  struct node * current = root.next;
+  while(current->socket != sock)
+    current = current->next;
+
+  int i;
+  for(i = 0; i < strlen(current->username); i++)
+    buf[i+1] = current->username[i];
+
+  buf[i+1] = ':';
+  buf[i+2] = ' ';
+  buf[i+2] = '\0';
+  int pre = strlen(buf);
+  if( (read_size = recv(sock , buf+pre, BUFSIZE-pre, 0)) < 0 )
+  {
+    printf("Error reading\n");
+    return -1;
+  }
+  buf[read_size+pre] = '\0';
+
+  current = root.next;
+  while(current) {
+    if(write(current->socket, buf, strlen(buf)+1) < 0) {
+      printf("Error writing\n");
+      return -1;
+    }
+    current = current->next;
+  }
+    
+  message = "Broadcast sent\n";
+  if(write(sock , message , strlen(message)+1) < 0) {
+    printf("Error writing\n");
+    return -1;
+  }
+
   return 0;
 }
 
 int private(int sock)
 {
+  char * buf;
+
+  buf = malloc(BUFSIZE);
+  memset(buf, '\0', BUFSIZE);
+  if(buf == NULL) {
+    printf("Error allocating list space\n");
+    return -1;
+  }
+
+  struct node * current = root.next;
+  while(current) {
+    strcat(buf,current->username);
+    strcat(buf,"\n");
+    current = current->next;
+  }
+
+  // send list
+  int i;
+  for(i = 0; i < 2; i++)
+  if(write(sock , buf , strlen(buf)+1) < 0) {
+    printf("Error writing\n");
+    free(buf);
+    return -1;
+  }
+
+  // get target user
+  int read_size; 
+  if( (read_size = recv(sock , buf, BUFSIZE, 0)) < 0 )
+  {
+    printf("Error reading\n");
+    return -1;
+  }
+  buf[read_size] = '\0';
+  
+  current = root.next;
+  while(current && strcmp(current->username, buf))
+    current = current->next;
+
+  int target = NULL;
+  if(current)
+    target = current->socket;
+
+  // append sending user's name to buffer
+  current = root.next;
+  while(current->socket != sock)
+    current = current->next;
+
+  buf[0] = 'D';
+  for(i = 0; i < strlen(current->username); i++)
+    buf[i+1] = current->username[i];
+
+  buf[i+1] = ':';
+  buf[i+2] = ' ';
+  buf[i+2] = '\0';
+  int pre = strlen(buf);
+
+  // read message
+  if( (read_size = recv(sock , buf+pre, BUFSIZE-pre, 0)) < 0 )
+  {
+    printf("Error reading\n");
+    return -1;
+  }
+  buf[read_size+pre] = '\0';
+
+  char * message = "Not sent";
+  if(target) {
+    if(write(target, buf, strlen(buf)+1) < 0) {
+      printf("Error writing\n");
+      return -1;
+    }
+    message = "Sent";
+  }
+  
+  for(i=0;i<2;i++)
+  if(write(sock , message , strlen(message)+1) < 0) {
+    printf("Error writing\n");
+    return -1;
+  }
+
+  free(buf);
   return 0;
 }
 
 int end(int sock)
 {
+  close(sock);
+  struct node * current = &root;
+
+  while(current->next->socket != sock)
+    current = current->next;
+
+  struct node * temp = current->next->next;
+  free(current->next);
+  current->next = temp;
+
   return 0;
 }
 
@@ -192,12 +331,12 @@ int login(int sock)
   //check name
   while(getline(&line, &len, fp) != -1) {
     line[strlen(line)-1] = '\0';
-    printf("%s\n", line);
     if(!strcmp(line, username)) {
       exists = 1;
 
       // associated password
       getline(&line, &len, fp);
+      line[strlen(line)-1] = '\0';
       break;
     }
     // skip password
@@ -209,13 +348,13 @@ int login(int sock)
   else
     message = "New user? Create password >> ";
 
-  if(write(sock , message , strlen(message)) < 0) {
+  if(write(sock , message , strlen(message)+1) < 0) {
     printf("Error writing\n");
     return -1;
   } 
 
   //password
-  if( read_size = recv(sock , password , 20, 0) < 0 )
+  if((read_size = recv(sock , password , 20, 0)) < 0 )
   {
     printf("Error reading\n");
     return -1;
@@ -225,7 +364,7 @@ int login(int sock)
   if(exists) {
     while(strcmp(line, password)) {
       message = "Invalid password.\nPlease enter again >> ";
-      if(write(sock , message , strlen(message)) < 0) {
+      if(write(sock , message , strlen(message)+1) < 0) {
         printf("Error writing\n");
         return -1;
       } 
@@ -238,10 +377,10 @@ int login(int sock)
 	    password[read_size] = '\0';
     }
     message = "Welcome!\n";
-    if(write(sock , message , strlen(message)) < 0) {
+    if(write(sock , message , strlen(message)+1) < 0) {
       printf("Error writing\n");
       return -1;
-    } 
+    }
   }
   else {
     fprintf(fp, username);
@@ -250,13 +389,16 @@ int login(int sock)
     fprintf(fp, "\n");
 
     message = "Created new user. Welcome!\n";
-    if(write(sock , message , strlen(message)) < 0) {
+    if(write(sock , message , strlen(message)+1) < 0) {
       printf("Error writing\n");
       return -1;
     } 
   }
 
-  struct node * current = &root;
+  struct node * current = root.next;
+  if(current == NULL)
+    printf("NULL!\n");
+
   while(current->socket != sock)
     current = current->next;
   strcpy(current->username, username);
